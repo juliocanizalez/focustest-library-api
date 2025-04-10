5b78bda54b837b69a8a9cb379cc3c15e2657fc18
Julio Canizalez <juliocanizalez@outlook.com>
2025-04-10 11:50:17 -0600

    Remove no-console

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,10 +4,8 @@ import config from './config';
 const connectDB = async (): Promise<void> => {
   try {
     await mongoose.connect(config.mongodbUri);
-    // eslint-disable-next-line no-console
     console.log('MongoDB Connected');
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.error('MongoDB connection error:', error);
     process.exit(1);
   }
